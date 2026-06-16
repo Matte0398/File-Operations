96a47da350bd6a451daf84619d84c15ace56194e
Matte <105994103+Matte0398@users.noreply.github.com>
2026-06-16 17:07:34 +0200

    Update file_operations.pl

--- file_operations.pl
+++ file_operations.pl
@@ -1,10 +1,10 @@
 #!/usr/bin/perl -w
 
-################################################################################
-## Description: Script to do some operations on a file, populated by default
+###############################################################################
+## Description: Script to do some operations on a file populated by default
 ##
 ## Author: Matteo Z.
-################################################################################
+###############################################################################
 
 use strict;
 use Getopt::Long;
@@ -15,170 +15,175 @@ use File::Copy;
 my ($create, $upper, $truncate, $remove, $read, $copy);
 my $size;
 my @new = ();
-my $upper_case;
+my $uppercase;
 my $answer;
 
 if (($#ARGV + 1) ne 2) {
-        &print_usage;
+    &print_usage;
 } else {
-        GetOptions(
-                "c|cr" => \$create,
-                "u|up" => \$upper,
-                "t|tr" => \$truncate,
-                "d|del" => \$remove,
-                "r|re" => \$read,
-                "C|cp" => \$copy,
-        );
+    GetOptions(
+        "c|cr" => \$create,
+        "u|up" => \$upper,
+        "t|tr" => \$truncate,
+        "d|del" => \$remove,
+        "r|re" => \$read,
+        "C|cp" => \$copy,
+    );
 
-        &create(@ARGV) if defined $create;
-        &upper_case(@ARGV) if defined $upper;
-        &truncate(@ARGV) if defined $truncate;
-        &remove(@ARGV) if defined $remove;
-        &read(@ARGV) if defined $read;
-        &copyItem(@ARGV) if defined $copy;
+    &create(@ARGV) if defined $create;
+    &uppercase(@ARGV) if defined $upper;
+    &truncate(@ARGV) if defined $truncate;
+    &remove(@ARGV) if defined $remove;
+    &read(@ARGV) if defined $read;
+    &copyItem(@ARGV) if defined $copy;
 
-        if (! $create and ! $upper and ! $truncate and ! $remove and ! $read and ! $copy) {
-                print color "bold red";
-                print "\nOption not recognized by this program!\n";
-                print color "reset";
-                &print_usage;
-        }
+    if (! $create and ! $upper and ! $truncate and ! $remove and ! $read and ! $copy) {
+        print color "bold red";
+        print "\nOption not recognized by this program!\n";
+        print color "reset";
+        &print_usage;
+    }
 }
 
 
 sub create {
-        if (! -f $_[0]) {
-                open(FH, ">", $_[0]) or die "Can't create the file!!\n";
-                print FH "unix is great os. unix is opensource. unix is free os.\n";
-                print FH "learn operating system.\n";
-                print FH "unixlinux which one you choose.\n";
-                close(FH);
-                print "File created and populated it with text! You can open it and read it!\n";
-        } else {
-                print "This file already exist!!\n";
-        }
+    if (! -f $_[0]) {
+        open(FH, ">", $_[0]) or die "Can't create the file!!\n";
+        print FH "unix is great os. unix is opensource. unix is free os.\n";
+        print FH "learn operating system.\n";
+        print FH "unixlinux which one you choose.\n";
+        close(FH);
+        print "File created and populated it with text! You can open it and read it!\n";
+    } else {
+        print "This file already exist!!\n";
+    }
 }
 
 
-sub upper_case {
-        $size = -s $_[0];    # file size in byte
+sub uppercase {
+    $size = -s $_[0];    # file size in byte
 
-        if (! -f $_[0]) {
-                &fileNotExist;
-        } else {
-                if ($size != 0) {
-                        open(FH, "<", $_[0]);
-                        while (<FH>) {
-                                $upper_case = uc($_);
-                                if ($_ ne $upper_case) {
-                                        $_ =~ tr/a-z/A-Z/;
-                                        push(@new, $_);
-                                } else {
-                                        next;
-                                }
-                        }
-                        close(FH);
+    if (! -f $_[0]) {
+        &fileNotExist;
+    } else {
+        if ($size != 0) {
+            open(FH, "<", $_[0]);
 
-                        open(FH, ">", $_[0]);
-                        print FH @new;
-                        close(FH);
+            while (<FH>) {
+                $uppercase = uc($_);
 
-                        if ($#new == 0) {
-                                print "The text of the file is already in upper-case!!\n";
-                        } else {
-                                print "The text of the file has been rewritten in upper-case!!\n";
-                        }
+                if ($_ ne $uppercase) {
+                    $_ =~ tr/a-z/A-Z/;
+                    push(@new, $_);
                 } else {
-                        print "This file is empty! Remove it and recreate it!\n";
+                    next;
                 }
+            }
+
+            close(FH);
+
+            open(FH, ">", $_[0]);
+            print FH @new;
+            close(FH);
+
+            if ($#new == 0) {
+                print "The text of the file is already in upper-case!!\n";
+            } else {
+                print "The text of the file has been rewritten in upper-case!!\n";
+            }
+        } else {
+            print "This file is empty! Remove it and recreate it!\n";
         }
+    }
 }
 
 
 sub truncate {
-        $size = -s $_[0];
+    $size = -s $_[0];
 
-        if (! -f $_[0]) {
-                &fileNotExist;
+    if (! -f $_[0]) {
+        &fileNotExist;
+    } else {
+        if ($size != 0) {
+            open(FH, "+<", $_[0]);                        # open the file both in reading and in writing
+            seek(FH, 0, 0);                               # pointer at the beginning of the file
+            truncate(FH, tell(FH));                       # truncate the file to where the pointer is located ('tell' to know where it is)
+            close(FH);
+            print "File content truncated!!\n";
         } else {
-                if ($size != 0) {
-                        open(FH, "+<", $_[0]);                        # open the file both in reading and in writing
-                        seek(FH, 0, 0);                               # pointer at the beginning of the file
-                        truncate(FH, tell(FH));                       # truncate the file to where the pointer is located ('tell' to know where it is)
-                        close(FH);
-                        print "File content truncated!!\n";
-                } else {
-                        print "This file is empty! Remove it and recreate it!\n";
-                }
+            print "This file is empty! Remove it and recreate it!\n";
         }
+    }
 }
 
 
 sub remove {
-        if (! -f $_[0]) {
-                print "Impossible to remove this file, because it has not been found!!\n";
-        } else {
-                unlink($_[0]);
-                print "File removed!!\n";
-        }
+    if (! -f $_[0]) {
+        print "Impossible to remove this file, because it has not been found!!\n";
+    } else {
+        unlink($_[0]);
+        print "File removed!!\n";
+    }
 }
 
 
 sub read {
-        $size = -s $_[0];
+    $size = -s $_[0];
 
-        if (! -f $_[0]) {
-                &fileNotExist;
+    if (! -f $_[0]) {
+        &fileNotExist;
+    } else {
+        if ($size != 0) {
+            open(FH, "<", $_[0]);
+            
+            while(<FH>) {
+                print $_;
+            }
+
+            close(FH);
         } else {
-                if ($size != 0) {
-                        open(FH, "<", $_[0]);
-                        while(<FH>) {
-                                print $_;
-                        }
-                        close(FH);
-                } else {
-                        print "This file is empty! Remove it and recreate it!\n";
-                }
+            print "This file is empty! Remove it and recreate it!\n";
         }
+    }
 }
 
 
 sub copyItem {
-        if (! -f $_[0]) {
-                &fileNotExist;
-        } else {
-                print "Type a filename where you want to copy the content of the file $_[0]: ";
-                $answer = <STDIN>;
+    if (! -f $_[0]) {
+        &fileNotExist;
+    } else {
+        print "Type a filename where you want to copy the content of the file $_[0]: ";
+        $answer = <STDIN>;
 
-                if ($answer ne "") {
-                        $answer =~ s/\n//g;
-                        copy($_[0], $answer) or die "Copy failed: $!";
-                        print "File content copied in $answer!!\n";
-                } else {
-                        print "The answer cannot be empty!";
-                }
+        if ($answer ne "") {
+            $answer =~ s/\n//g;
+            copy($_[0], $answer) or die "Copy failed: $!";
+            print "File content copied in $answer!!\n";
+        } else {
+            print "The answer cannot be empty!";
         }
+    }
 }
 
 
 sub fileNotExist {
-        print color "bold red";
-        print "\nWarning!! You should create before the file!\n";
-        print color "reset";
-        sleep 1;
-        &print_usage;
+    print color "bold red";
+    print "\nWarning!! You should create before the file!\n";
+    print color "reset";
+    sleep 1;
+    &print_usage;
 }
 
 
 sub print_usage {
-  print color "green";
-  print "\nUsage:\n\n";
-  print color "reset";
-  print "  $0 [<option>] [<file>]\n\n";
-  print color "green";
-  print "Options:\n\n";
-  print color "reset";
-  print<<EOF;
+    print color "green";
+    print "\nUsage:\n\n";
+    print color "reset";
+    print "  $0 [<option>] [<file>]\n\n";
+    print color "green";
+    print "Options:\n\n";
+    print color "reset";
+    print << EOF;
      '-c|--cr' = to create the file\n\n
      '-u|--up' = to transform the file content in upper-case\n\n
      '-t|--tr' = to truncate the file content\n\n
